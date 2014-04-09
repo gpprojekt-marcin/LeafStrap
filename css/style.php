81db8e21e5cca9c98b5254f9e69fb36760ed5cc1
Gontar <marcin.gontarek@gmail.com>
2014-04-09 10:24:13 +0200

    Core and LayerManager

--- css/style.php
+++ css/style.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(-1);
+
 if (isset($_GET['compress'])) {
 	$options = array( 'compress'=>true );
 } else {
